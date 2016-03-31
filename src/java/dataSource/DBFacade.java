1a2823596935916be45fc7c1bdb6c0c8359b2939
Plamen <Plamen@10.20.153.92>
2016-03-31 12:47:59 +0200

    dbfacade foundations

--- src/java/dataSource/DBFacade.java
+++ src/java/dataSource/DBFacade.java
@@ -5,10 +5,33 @@
  */
 package dataSource;
 
+import Entity.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 /**
  *
  * @author Plamen
  */
 public class DBFacade {
+
+    public static DBFacade getInstance() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    private DataMapper dm; 
+    private Connection con;
     
+    private static DBFacade instance;
+    
+    private DBFacade(){
+                  dm = new DataMapper();
+		  con 	= DBConnector.getInstance().getConnection();
+                  DBFacade facade = new DBFacade();
+    }
+    
+   private void addBuilding(Building building){
+    }
 }
+    
+
