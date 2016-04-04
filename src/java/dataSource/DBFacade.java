a59512e80a2bb03cecb3b356d1e8bfeb97f4adf9
Mircea <Mircea@10.59.130.17>
2016-04-04 12:23:03 +0200

    DomainFacade class

--- src/java/dataSource/DBFacade.java
+++ src/java/dataSource/DBFacade.java
@@ -5,32 +5,47 @@
  */
 package dataSource;
 
-import Entity.*;
+
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 /**
  *
  * @author Plamen
  */
 public class DBFacade {
 
-    public static DBFacade getInstance() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    private DataMapper dm; 
-    private Connection con;
-    
+    private final Connection con;
+    private Statement statement;
     private static DBFacade instance;
     
-    private DBFacade(){
-                  dm = new DataMapper();
-		  con 	= DBConnector.getInstance().getConnection();
-                  DBFacade facade = new DBFacade();
-    }
     
-   private void addBuilding(Building building){
+    public DBFacade(){
+            con = DBConnector.getInstance().getConnection();
+	  }
+    
+    public static DBFacade getInstance()
+	  {
+		  if(instance == null)
+			  instance = new DBFacade();
+		  return instance;
+	  }
+    
+    
+    
+   public void addBuilding(String name, String address, int pno, String size){
+       try { 	
+            statement = con.createStatement();
+            String sql = "INSERT into buildings (cname,address,parcelno,size) values (?,?,?,?,?);";
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, name);
+            stmt.setString(2, address);
+            stmt.setInt(3, pno);
+            stmt.setString(4, size);
+            stmt.execute();
+        }catch (SQLException ex){}
+        
     }
 }
     
