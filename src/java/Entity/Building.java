128919663103582a286106e5ce20fd84a1f690e1
Mircea <Mircea@10.59.130.17>
2016-04-07 11:24:20 +0200

    Building class final

--- src/java/Entity/Building.java
+++ src/java/Entity/Building.java
@@ -7,8 +7,59 @@ package Entity;
 
 /**
  *
- * @author Plamen
+ * @author Mircea
  */
 public class Building {
+    String name;
+    String address;
+    int pno;
+    String size;
+    
+    public Building(){}
+
+    public Building(String name, String address, int pno, String size) {
+        this.name = name;
+        this.address = address;
+        this.pno = pno;
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getPno() {
+        return pno;
+    }
+
+    public void setPno(int pno) {
+        this.pno = pno;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    @Override
+    public String toString() {
+        return "Building{" + "name=" + name + ", address=" + address + ", pno=" + pno + ", size=" + size + '}';
+    }
+    
     
 }
