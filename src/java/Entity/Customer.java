ae8e12476ac35cc633f88ea352dafe0e11f46849
Berkant <Berkant@10.20.155.177>
2016-04-13 12:20:29 +0200

    Update

--- src/java/Entity/Customer.java
+++ src/java/Entity/Customer.java
@@ -5,62 +5,133 @@
  */
 package Entity;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Mircea
  */
 public class Customer {
-    String cuname;
-    String email;
-    int telephone;
-    String caddress;
+    int cid;
+    String username;
+    String password;
+    String firstname;
+    String lastname;
+    String mail;
+    String tel;
+    String city;
+    String address;
+    String zip;
+    ArrayList<Building> buildings;
 
-    public Customer(String cuname, String email, int telephone, String caddress) {
-        this.cuname = cuname;
-        this.email = email;
-        this.telephone = telephone;
-        this.caddress = caddress;
+    public Customer(int cid, String username, String password, String firstname, String lastname, String mail, String tel, String city, String address, String zip) {
+        this.cid = cid;
+        this.username = username;
+        this.password = password;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.mail = mail;
+        this.tel = tel;
+        this.city = city;
+        this.address = address;
+        this.zip = zip;
     }
 
     public Customer() {
     }
 
-    public String getCuname() {
-        return cuname;
+    public int getCid() {
+        return cid;
     }
 
-    public void setCuname(String cuname) {
-        this.cuname = cuname;
+    public void setCid(int cid) {
+        this.cid = cid;
     }
 
-    public String getEmail() {
-        return email;
+    public String getUsername() {
+        return username;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
-    public int getTelephone() {
-        return telephone;
+    public String getPassword() {
+        return password;
     }
 
-    public void setTelephone(int telephone) {
-        this.telephone = telephone;
+    public void setPassword(String password) {
+        this.password = password;
     }
 
-    public String getCaddress() {
-        return caddress;
+    public String getFirstname() {
+        return firstname;
     }
 
-    public void setCaddress(String caddress) {
-        this.caddress = caddress;
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public ArrayList<Building> getBuildings() {
+        return buildings;
+    }
+
+    public void addBuildings(Building building) {
+        buildings.add(building);
     }
 
     @Override
     public String toString() {
-        return "Customer{" + "cuname=" + cuname + ", email=" + email + ", telephone=" + telephone + ", caddress=" + caddress + '}';
+        return "Customer{" + "cid=" + cid + ", username=" + username + ", password=" + password + ", firstname=" + firstname + ", lastname=" + lastname + ", mail=" + mail + ", tel=" + tel + ", city=" + city + ", address=" + address + ", zip=" + zip + ", buildings=" + buildings + '}';
     }
-    
+
     
 }
