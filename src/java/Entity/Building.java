0b46d970b8cb3421e0e6f62a4fbafce85f98784d
Berkant <Berkant@10.20.155.177>
2016-04-13 12:20:15 +0200

    Update

--- src/java/Entity/Building.java
+++ src/java/Entity/Building.java
@@ -5,33 +5,75 @@
  */
 package Entity;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Mircea
  */
 public class Building {
-    String name;
-    String address;
-    int pno;
-    String size;
-    String cname;
     
-    public Building(){}
-
-    public Building(String name, String address, int pno, String size, String cname) {
-        this.name = name;
+    int cid;
+    int bid;
+    String bname;
+    String city;
+    String address;
+    String zip;   
+    String year;
+    String size;
+    Boolean roof;
+    String usagecomment;
+    ArrayList<Documents> documents;
+    ArrayList<Bimages> bimages;
+    ArrayList<Floor> floors;
+    ArrayList<Report> reports;
+    
+    public Building(int cid, int bid, String bname, String city, String address, String zip, String year, String size, Boolean roof, String usagecomment) {
+        this.cid = cid;
+        this.bid = bid;
+        this.bname = bname;
+        this.city = city;
         this.address = address;
-        this.pno = pno;
+        this.zip = zip;
+        this.year = year;
         this.size = size;
-        this.cname = cname;
+        this.roof = roof;
+        this.usagecomment = usagecomment;
     }
 
-    public String getName() {
-        return name;
+    public Building() {
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public int getCid() {
+        return cid;
+    }
+
+    public void setCid(int cid) {
+        this.cid = cid;
+    }
+
+    public int getBid() {
+        return bid;
+    }
+
+    public void setBid(int bid) {
+        this.bid = bid;
+    }
+
+    public String getBname() {
+        return bname;
+    }
+
+    public void setBname(String bname) {
+        this.bname = bname;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
     }
 
     public String getAddress() {
@@ -42,12 +84,20 @@ public class Building {
         this.address = address;
     }
 
-    public int getPno() {
-        return pno;
+    public String getZip() {
+        return zip;
     }
 
-    public void setPno(int pno) {
-        this.pno = pno;
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
     }
 
     public String getSize() {
@@ -57,17 +107,59 @@ public class Building {
     public void setSize(String size) {
         this.size = size;
     }
-    public String getCname() {
-        return cname;
+
+    public Boolean getRoof() {
+        return roof;
     }
-    public void setCname(String cname) {
-        this.cname = cname;
+
+    public void setRoof(Boolean roof) {
+        this.roof = roof;
+    }
+
+    public String getUsagecomment() {
+        return usagecomment;
+    }
+
+    public void setUsagecomment(String usagecomment) {
+        this.usagecomment = usagecomment;
+    }
+
+    public ArrayList<Documents> getDocuments() {
+        return documents;
+    }
+
+    public void addDocuments(Documents document) {
+        documents.add(document);
+    }
+
+    public ArrayList<Bimages> getBimages() {
+        return bimages;
+    }
+
+    public void addBimages(Bimages bimage) {
+        bimages.add(bimage);
+    }
+
+    public ArrayList<Floor> getFloors() {
+        return floors;
+    }
+
+    public void addFloors(Floor Floor) {
+        floors.add(Floor);
+    }
+
+    public ArrayList<Report> getReports() {
+        return reports;
+    }
+
+    public void addReports(Report report) {
+        reports.add(report);
     }
 
     @Override
     public String toString() {
-        return "Building{" + "name=" + name + ", address=" + address + ", pno=" + pno + ", size=" + size + ", cname=" + cname + '}';
+        return "Building{" + "cid=" + cid + ", bid=" + bid + ", bname=" + bname + ", city=" + city + ", address=" + address + ", zip=" + zip + ", year=" + year + ", size=" + size + ", roof=" + roof + ", usagecomment=" + usagecomment + ", documents=" + documents + ", bimages=" + bimages + ", floors=" + floors + ", reports=" + reports + '}';
     }
-    
+
     
 }
