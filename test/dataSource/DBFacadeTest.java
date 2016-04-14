60db81a7ef8a0a32cbe738f61a359e195bae9a52
Mircea <Mircea@10.59.130.17>
2016-04-14 13:55:21 +0200

    Commented DBFacadeTest until new tests

--- test/dataSource/DBFacadeTest.java
+++ test/dataSource/DBFacadeTest.java
@@ -25,67 +25,67 @@ public class DBFacadeTest {
         dbf = new DBFacade(TestDBConnector.getTestInstance().getTestConnection());
     }
 
-    @Test
-    public void testBuilding() {
-        Customer ct = new Customer();
-        String name = "Henrik";
-        String email = "henrik@genericmail.dk";
-        int tel = 52222222;
-        String address = "The strasse";
-        dbf.addCustomer(name, email, tel, address);
-        
-        Building bd = new Building();
-        String bname = "Kommune";
-        String baddress = "Noname Boulevard 64";
-        int pno = 2000;
-        String size = "Big";
-        String cname = "Henrik";
-        dbf.addBuilding(bname, baddress, pno, size, cname);
-        bd = dbf.getBuilding(bname);
-        System.out.println(bd.getName());
-        assertTrue(bname.equals(bd.getName()));
-        
-    }
-
-    @Test
-    public void testCustomer() {
-        Customer ct = new Customer();
-        String name = "Henrik";
-        String email = "henrik@genericmail.dk";
-        int tel = 52222222;
-        String address = "The strasse";
-        dbf.addCustomer(name, email, tel, address);
-        ct = dbf.getCustomer(name);
-        System.out.println(ct.getCuname());
-        assertTrue(name.equals(ct.getCuname()));
-    }
-    
-    @Test
-    public void testReport() {
-        Customer ct = new Customer();
-        String cuname = "Henrik";
-        String email = "henrik@genericmail.dk";
-        int tel = 52222222;
-        String address = "The strasse";
-        dbf.addCustomer(cuname, email, tel, address);
-        
-        Building bd = new Building();
-        String name = "Kommune";
-        String baddress = "Noname Boulevard 64";
-        int pno = 2000;
-        String size = "Big";
-        String cname = "Henrik";
-        dbf.addBuilding(name, baddress, pno, size, cname);
-        
-        Report rp = new Report();
-        int reportno = 1;
-        String rdate = "today";
-        String bname = "Kommune";
-        String rcomment = "A very nice building indeed";
-        dbf.addReport(reportno, rdate, bname, rcomment);
-        rp = dbf.getReport(reportno);
-        System.out.println(rp);
-        assertTrue(reportno == rp.getReportno());
-    }
+//    @Test
+//    public void testBuilding() {
+//        Customer ct = new Customer();
+//        String name = "Henrik";
+//        String email = "henrik@genericmail.dk";
+//        int tel = 52222222;
+//        String address = "The strasse";
+//        dbf.addCustomer(name, email, tel, address);
+//        
+//        Building bd = new Building();
+//        String bname = "Kommune";
+//        String baddress = "Noname Boulevard 64";
+//        int pno = 2000;
+//        String size = "Big";
+//        String cname = "Henrik";
+//        dbf.addBuilding(bname, baddress, pno, size, cname);
+//        bd = dbf.getBuilding(bname);
+//        System.out.println(bd.getName());
+//        assertTrue(bname.equals(bd.getName()));
+//        
+//    }
+//
+//    @Test
+//    public void testCustomer() {
+//        Customer ct = new Customer();
+//        String name = "Henrik";
+//        String email = "henrik@genericmail.dk";
+//        int tel = 52222222;
+//        String address = "The strasse";
+//        dbf.addCustomer(name, email, tel, address);
+//        ct = dbf.getCustomer(name);
+//        System.out.println(ct.getCuname());
+//        assertTrue(name.equals(ct.getCuname()));
+//    }
+//    
+//    @Test
+//    public void testReport() {
+//        Customer ct = new Customer();
+//        String cuname = "Henrik";
+//        String email = "henrik@genericmail.dk";
+//        int tel = 52222222;
+//        String address = "The strasse";
+//        dbf.addCustomer(cuname, email, tel, address);
+//        
+//        Building bd = new Building();
+//        String name = "Kommune";
+//        String baddress = "Noname Boulevard 64";
+//        int pno = 2000;
+//        String size = "Big";
+//        String cname = "Henrik";
+//        dbf.addBuilding(name, baddress, pno, size, cname);
+//        
+//        Report rp = new Report();
+//        int reportno = 1;
+//        String rdate = "today";
+//        String bname = "Kommune";
+//        String rcomment = "A very nice building indeed";
+//        dbf.addReport(reportno, rdate, bname, rcomment);
+//        rp = dbf.getReport(reportno);
+//        System.out.println(rp);
+//        assertTrue(reportno == rp.getReportno());
+//    }
     
 }
