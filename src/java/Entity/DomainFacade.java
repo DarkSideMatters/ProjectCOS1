c9acbe0aff07015cb05e50f1b55b13a6777d52f5
Plamen <Plamen@10.20.171.179>
2016-04-20 16:41:03 +0200

    Domain Facade with edit method

--- src/java/Entity/DomainFacade.java
+++ src/java/Entity/DomainFacade.java
@@ -113,6 +113,42 @@ public class DomainFacade{
         dbf.deleteBuilding(bid);
     }
     
+    public void editAdmin(int aid, String username, String password, String firstname, String lastname, String mail, String tel) {
+        if(!username.equals(""))
+            dbf.updateUsernameAdmin(aid,username);
+        if(!password.equals(""))
+            dbf.updatePassAdmin(aid,password);
+        if(!firstname.equals(""))
+            dbf.updateFNAdmin(aid,firstname);
+        if(!lastname.equals(""))
+            dbf.updateLNAdmin(aid,lastname);
+        if(!mail.equals(""))
+            dbf.updateMailAdmin(aid,mail);
+        if(!tel.equals(""))
+            dbf.updateTelAdmin(aid,tel);
+            }
+    
+    public void editCustomer(int cid, String username, String password, String firstname, String lastname, String mail, String tel, String city, String address, String zip) {
+        if(!username.equals(""))
+            dbf.updateUsernameCustomer(cid,username);
+        if(!password.equals(""))
+            dbf.updatePassCustomer(cid,password);
+        if(!firstname.equals(""))
+            dbf.updateFNCustomer(cid,firstname);
+        if(!lastname.equals(""))
+            dbf.updateLNCustomer(cid,lastname);
+        if(!mail.equals(""))
+            dbf.updateMailCustomer(cid,mail);
+        if(!tel.equals(""))
+            dbf.updateTelCustomer(cid,tel);
+        if(!city.equals(""))
+            dbf.updateCityCustomer(cid,city);
+        if(!address.equals(""))
+            dbf.updateAddressCustomer(cid,address);
+        if(!zip.equals(""))
+            dbf.updateZipCustomer(cid, zip);
+            
+    }    
     public void editBuilding(int bid, String bname, String bcity, String baddress, String bzip, String year, String size) {
         if(!bname.equals(""))
             dbf.updateBnameBuilding(bid,bname);
@@ -130,13 +166,115 @@ public class DomainFacade{
     }
 
     
+    public void editFloor(int fid, String fname, String fpath) {
+        if(!fname.equals(""))
+            dbf.updateFnameFloor(fid,fname);
+        if(!fpath.equals(""))
+            dbf.updateFpathFloor(fid,fpath);   
+    }
+    
+    public void editRoom(int rid, String rname, String size) {
+        if(!rname.equals(""))
+            dbf.updateRnameRoom(rid,rname);
+        if(!size.equals(""))
+            dbf.updateSizeRoom(rid,size);       
+    }
+
+    public void editBReport(int brepid, String brepnum, String brdate, String usagec, String extwalls, String bconmanager, String grade){
+        if(!brepnum.equals(""))
+            dbf.updateBrepnumBReport(brepid,brepnum);
+        if(!brdate.equals(""))
+            dbf.updateBrdateBReport(brdate,brepnum);
+        if(!usagec.equals(""))
+            dbf.updateUsagecBReport(brepid,usagec);
+        if(!extwalls.equals(""))
+            dbf.updateExtwallsBReport(brepid,extwalls);
+        if(!bconmanager.equals(""))
+            dbf.updateBconManagerBReport(brepid,bconmanager);
+        if(!grade.equals(""))
+            dbf.updateGradeBReport(brepid,grade);
+    }
+
+    public void editRReport(int rrepid, String rrepnum, String rrdate, String roomc, String dmg, String moist, String rot, String mould, String fire, String other, String windoor, String moistscan, String moistpoint, String recommendation, String rconmanager ){
+        if(!rrepnum.equals(""))
+            dbf.updateRrepnumRReport(rrepid,rrepnum);
+        if(!rrdate.equals(""))
+            dbf.updateRRdateRReport(rrepid,rrdate);
+        
+        boolean damage;
+        if(dmg.equals("yes")){
+                damage=true;
+                dbf.updateDamageRReport(rrepid,damage);
+        }
+        if(dmg.equals("no")){
+                damage=false;
+                dbf.updateDamageRReport(rrepid,damage);
+        }
+        boolean rmoist;
+        if(moist.equals("yes")){
+                rmoist=true;
+                dbf.updateMoistRReport(rrepid,rmoist);
+        }
+        if(moist.equals("no")){
+                rmoist=false;
+                dbf.updateMoistRReport(rrepid,rmoist);
+        }
+        boolean rrot;
+        if(rot.equals("yes")){
+                rrot=true;
+                dbf.updateRotRReport(rrepid,rrot);
+        }
+        if(rot.equals("no")){
+                rrot=false;
+                dbf.updateRotRReport(rrepid,rrot);
+        }
+        boolean rmould;
+        if(mould.equals("yes")){
+                rmould=true;
+                dbf.updateMouldRReport(rrepid,rmould);
+        }
+        if(mould.equals("no")){
+                rmould=false;
+                dbf.updateMouldRReport(rrepid,rmould);
+        }
+        boolean rfire;
+        if(fire.equals("yes")){
+                rfire=true;
+                dbf.updateFireRReport(rrepid,rfire);
+        }
+        if(fire.equals("no")){
+                rfire=false;
+                dbf.updateFireRReport(rrepid,rfire);
+        }
+        boolean rother;
+        if(other.equals("yes")){
+                rother=true;
+                dbf.updateOtherDMGRReport(rrepid,rother);
+        }
+        if(dmg.equals("no")){
+                rother=false;
+                dbf.updateOtherDMGRReport(rrepid,rother);
+        }
+        if(!windoor.equals(""))
+            dbf.updateWindoorRReport(rrepid,windoor);
+        if(!moistscan.equals(""))
+            dbf.updateMoistScanRReport(rrepid,moistscan);
+        if(!moistpoint.equals(""))
+            dbf.updateMoistPointRReport(rrepid,moistpoint);
+        if(!recommendation.equals(""))
+            dbf.updateRecommendationRReport(rrepid,recommendation);
+        if(!rconmanager.equals(""))
+            dbf.updateRConManagerRReport(rrepid,rconmanager);
+        
+        
+    }    
 
     public void addAdmin(String ausrn, String apwd, String afn, String aln, String aemail, String atel) {
         Admin ad = new Admin(ausrn,apwd,afn,aln,aemail,atel);
         dbf.addAdmin(ad);
                 
     }
-
+    
     public void readAdmins(Accounts acc) {
         dbf.readAdmins(acc);
     }
